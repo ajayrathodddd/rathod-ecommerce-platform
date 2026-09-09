784520c8092c7c9140a6f9988353562e9c4f947d
Ajay Rathod <ajayrathodddd1@gmail.com>
2026-09-10 01:28:42 +0530

    Fix ProductConstants import case

--- frontend/app/src/actions/ProductAction.js
+++ frontend/app/src/actions/ProductAction.js
@@ -17,7 +17,7 @@ import {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
   PRODUCT_UPDATE_RESET,
-} from "../constants/productConstants";
+} from "../constants/ProductConstants";
 
 export const listProducts = () => async (dispatch) => {
   try {
