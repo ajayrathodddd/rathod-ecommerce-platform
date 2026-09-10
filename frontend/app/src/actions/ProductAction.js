042c1f65e829811a567d5e413df5e8fa7cc036e1
aj <aj18@gmail.com>
2026-09-10 14:52:01 +0530

    Fix product details and message handling

--- frontend/app/src/actions/ProductAction.js
+++ frontend/app/src/actions/ProductAction.js
@@ -42,7 +42,9 @@ export const listProducts = () => async (dispatch) => {
 export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/product/${id}`);
+   const { data } = await axios.get(
+  `https://rathod-ecommerce-platform.onrender.com/api/product/${id}/`
+);
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data,
--- frontend/app/src/components/Message.jsx
+++ frontend/app/src/components/Message.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Alert } from 'react-bootstrap';
+import { Alert } from "react-bootstrap";
 
-function Message(variant, children) {
-  return <Alert variant={variant}>{children}</Alert>
+function Message({ variant, children, onClose }) {
+  return (
+    <Alert variant={variant} onClose={onClose} dismissible={!!onClose}>
+      {children}
+    </Alert>
+  );
 }
 
 export default Message;
